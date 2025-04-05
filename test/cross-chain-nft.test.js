4b2d2db86e10d8dd1dba1c32a09ffd942fc85d2f
chenbin <you@example.com>
2025-04-05 19:33:08 +0800

    添加cross-chain-nft测试用例，验证用户在不同链之间铸造和锁定NFT的功能

---
+++ test/cross-chain-nft.test.js
@@ -0,0 +1,13 @@
+//prepqre variables:contract,account
+
+//source chain -?dest chain
+//test if user can mint a nft from nft contract successfuly
+
+//test if user can lock the nft in the pool and send ccip message on source chain
+
+//test if user can get a wrapped nft in dest chain
+
+//dest chian -> source chain
+//test if user can burn the wnft and send ccip message on dest chain
+
+//test if user have the nft unlockes on source chain
