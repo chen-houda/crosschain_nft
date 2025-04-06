c324e01f632efa646782bc822b20c48685f3ae84
chenbin <you@example.com>
2025-04-07 01:08:20 +0800

    更新NFTPool合约以使用EVMExtraArgsV1，修改cross-chain-nft测试用例以提高可读性和功能验证

--- test/cross-chain-nft.test.js
+++ test/cross-chain-nft.test.js
@@ -1,8 +1,36 @@
 //prepqre variables:contract,account
+const { ethers, deployments, getNamedAccounts, network } = require("hardhat")
+const { assert, expect } = require("chai")
 
+let firstAccount
+let nft
+let wnft
+let poolLnU
+let poolMnB
+let chainSelector
+before(async function () {
+    firstAccount = (await getNamedAccounts()).firstAccount
+    await deployments.fixture(["all"])
+    nft = await ethers.getContract("MyToken", firstAccount)
+    wnft = await ethers.getContract("WrappedMyToken", firstAccount)
+    poolLnU = await ethers.getContract("NFTPoolLockAndRelease", firstAccount)
+    poolMnB = await ethers.getContract("NFTPoolBurnAndMint", firstAccount)
+    ccipLocalSimulator = await ethers.getContract("CCIPLocalSimulator", firstAccount)
+    chainSelector = (await ccipLocalSimulator.configuration()).chainSelector_
+})
 //source chain -?dest chain
 //test if user can mint a nft from nft contract successfuly
-
+describe("test if the nft can be minted successfully",
+    async function () {
+        it("test if the owner of nft is minter",
+            async function () {
+                // get nft
+                await nft.safeMint(firstAccount)
+                // check the owner
+                const ownerOfNft = await nft.ownerOf(0)
+                expect(ownerOfNft).to.equal(firstAccount)
+            })
+    })
 //test if user can lock the nft in the pool and send ccip message on source chain
 
 //test if user can get a wrapped nft in dest chain
