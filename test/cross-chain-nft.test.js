257484b9b8e558f2b3457db7e9221144d152a51f
chenbin <you@example.com>
2025-04-08 23:14:54 +0800

    优化cross-chain-nft测试用例，移除冗余代码并增加新的测试用例以验证NFT的锁定和解锁功能

--- test/cross-chain-nft.test.js
+++ test/cross-chain-nft.test.js
@@ -39,22 +39,15 @@ describe("test if user can lock the nft in the pool and send ccip message on sou
         // transfer NFT from source chain to dest chain, check if the nft is locked
         it("transfer NFT from source chain to dest chain, check if the nft is locked",
             async function () {
+
                 await nft.approve(poolLnU.target, 0)
                 await ccipLocalSimulator.requestLinkFromFaucet(poolLnU.target, ethers.parseEther("10"))
                 await poolLnU.lockAndSendNFT(0, firstAccount, chainSelector, poolMnB.target)
                 const newOwner = await nft.ownerOf(0)
-
-                // console.log("firstAccount ", firstAccount)
-                // const aa = await poolLnU.test(firstAccount)
-                // console.log("Result:", aa);
-                // const bb = await poolLnU.test2(firstAccount)
-                // console.log("Result2:", bb);
-                //   const cc = await poolLnU.test3(0, firstAccount, chainSelector, poolMnB.target)
-                // console.log("Result3:", cc);
-
                 expect(newOwner).to.equal(poolLnU.target)
             }
         )
+        //test if user can get a wrapped nft in dest chain
         it("test if user can get a wrapped nft in dest chain",
             async function () {
                 const newOwner = await wnft.ownerOf(0)
@@ -64,11 +57,28 @@ describe("test if user can lock the nft in the pool and send ccip message on sou
     }
 )
 
-
-
-//test if user can get a wrapped nft in dest chain
-
 //dest chian -> source chain
-//test if user can burn the wnft and send ccip message on dest chain
+describe("dest chian -> source chain",
+    async function () {
+        //test if user can burn the wnft and send ccip message on dest chain
+        it("test if user can burn the wnft and send ccip message on dest chain",
+            async function () {
+                await wnft.approve(poolMnB.target, 0)
+                await ccipLocalSimulator.requestLinkFromFaucet(poolMnB.target, ethers.parseEther("10"))
+                await poolMnB.burnAndMint(0, firstAccount, chainSelector, poolLnU.target)
+                const totalSuply = await wnft.totalSupply()
+                expect(totalSuply).to.equal(0)
+            })
+        //test if user have the nft unlockes on source chain
+        it("test if user have the nft unlockes on source chain",
+            async function () {
+                const owner = await nft.ownerOf(0)
+                expect(owner).to.equal(firstAccount)
+            }
+        )
 
-//test if user have the nft unlockes on source chain
+    })
+
+
+
+
