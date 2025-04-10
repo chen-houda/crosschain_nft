322b58b053f95f181d8ba42f5d1e7129ef4ce9d3
chenbin <you@example.com>
2025-04-10 19:54:26 +0800

    添加网络配置支持，更新hardhat配置以使用加密的环境变量

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,12 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ethers");
 require("hardhat-deploy");
 require("hardhat-deploy-ethers");
+//require('dotenv').config()
+require('@chainlink/env-enc').config()
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const AMOY_RPC_URL = process.env.AMOY_URL
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -24,6 +30,19 @@ module.exports = {
     },
     secondAccount: {
       default: 1
+    }
+  }, networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,//sepolia chain id
+      blockConfirmations: 6
+    },
+    sepolia: {
+      url: AMOY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 80002,//sepolia chain id
+      blockConfirmations: 6
     }
   }
 };
