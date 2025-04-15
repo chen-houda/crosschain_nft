526de3cdcc1403cd2200710645e1cfc0d6f322b2
chenbin <you@example.com>
2025-04-15 23:36:29 +0800

    添加NFT检查任务，简化任务管理并更新hardhat配置以加载任务文件

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,9 @@ require("hardhat-deploy");
 require("hardhat-deploy-ethers");
 //require('dotenv').config()
 require('@chainlink/env-enc').config()
-
+require("./task")
+//这会触发 index.js 文件的执行，从而加载并注册所有任务文件中的任务
+//index.js 的作用是作为任务文件的入口点，统一加载并导出所有任务文件中的任务定义。通过这种方式，你可以在 hardhat.config.js 中只需加载一次 index.js，就能注册所有任务，简化了任务的管理和使用
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const AMOY_RPC_URL = process.env.AMOY_URL
---
+++ task/index.js
@@ -0,0 +1,2 @@
+// deploy-fundme.js 和 interact-fundme.js 中定义的内容导出，以便其他文件可以通过 require 引入并使用这些任务。
+exports.checkNft = require("./check-nft")
